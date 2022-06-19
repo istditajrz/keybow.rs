3f9f771f844d301f40f8490f92b30269fe812ed0
istditarjz <istditarjz@gmail.com>
2022-06-19 20:59:02 +0100

    gpio-cdev update

--- src/lib.rs
+++ src/lib.rs
@@ -1,76 +1,4 @@
 #![allow(dead_code)]
-use gpio::{self, GpioIn};
-
-struct DummyPin<F> {
-    value: F
-}
-
-impl<F> DummyPin<F> {
-    fn new(value: F) -> DummyPin<F> {
-        Self { value }
-    }
-}
-
-impl<V, F> gpio::GpioIn for DummyPin<F>
-where
-    V: Into<gpio::GpioValue>,
-    F: Fn() -> V
-{
-    type Error = std::io::Error;
-
-    fn read_value(&mut self) -> Result<gpio::GpioValue, Self::Error> {
-        Ok((self.value)().into())
-    }
-}
-
-pub struct Key {
-    pin: Box<dyn gpio::GpioIn<Error = std::io::Error>>, // GPIO connection to be read
-    // led_index: usize,                                // Led support to be added in the future
-    function: Option<Box<dyn Fn(bool)>>                 // The function called on the key on a state change
-}
-
-impl Key {
-    pub fn new_from_index(index: usize, mini: bool, function: Option<Box<dyn Fn(bool)>>) -> Result<Self, std::io::Error> {
-        let pin = {
-            if mini {
-                match index {
-                    0 => GpioV2Pins::P1_38 as u16,
-                    1 => GpioV2Pins::P1_11 as u16,
-                    2 => GpioV2Pins::P1_18 as u16,
-                    _ => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Invalid key index for device (Keybow mini)"))
-                }
-            } else {
-                match index {
-                    0  => GpioV2Pins::P1_38 as u16,
-                    1  => GpioV2Pins::P1_31 as u16,
-                    2  => GpioV2Pins::P1_15 as u16,
-                    3  => GpioV2Pins::P1_11 as u16,
-                    4  => GpioV2Pins::P1_36 as u16,
-                    5  => GpioV2Pins::P1_32 as u16,
-                    6  => GpioV2Pins::P1_18 as u16,
-                    7  => GpioV2Pins::P1_13 as u16,
-                    8  => GpioV2Pins::P1_37 as u16,
-                    9  => GpioV2Pins::P1_33 as u16,
-                    10 => GpioV2Pins::P1_29 as u16,
-                    11 => GpioV2Pins::P1_16 as u16,
-                    _ => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Invalid key index for device (Keybow mini)"))
-                }
-            }
-        };
-        Ok(Self {
-            pin: Box::new(gpio::sysfs::SysFsGpioInput::open(pin)?),
-            function
-        })
-    }
-}
-
-impl gpio::GpioIn for Key {
-    type Error = std::io::Error;
-
-    fn read_value(&mut self) -> Result<gpio::GpioValue, Self::Error> {
-        self.pin.read_value()
-    }
-}
 
 enum GpioV2Pins {
     P1_03 = 2,  P1_05 = 3,  P1_07 = 4,  P1_08 = 14,
@@ -84,56 +12,103 @@ enum GpioV2Pins {
 }
 
 pub struct Keybow {
-    mapping_table: Vec<Key>, // Collection of keys
+    mapping_table: Vec<(gpio_cdev::Line, Box<dyn Fn(bool)>)>, // Collection of keys
     states: Vec<bool>,       // Last state of the keys to compare against
     mini: bool,              // Whether it is the keybow mini (3 key) or full (12 key)
+    _chip: gpio_cdev::Chip
 }
 
 impl Keybow {
-    pub fn new() -> Self {
-        Self {
+    pub fn new<P: AsRef<std::path::Path>>(path: P) -> Result<Self, std::io::Error> {
+        Ok(Self {
             mapping_table: Vec::with_capacity(3),
             states: Vec::with_capacity(3),
-            mini: false
-        }
+            mini: false,
+            _chip: match gpio_cdev::Chip::new(path) {
+                Ok(c)  => c,
+                Err(e) => return Err(std::io::Error::new(std::io::ErrorKind::Other, e))
+            }
+        })
     }
 
-    pub fn new_mini() -> Self {
-        Self {
+    pub fn new_mini<P: AsRef<std::path::Path>>(path: P) -> Result<Self, std::io::Error> {
+        Ok(Self {
             mapping_table: Vec::with_capacity(3),
             states: Vec::with_capacity(3),
-            mini: true
-        }
+            mini: true,
+            _chip: match gpio_cdev::Chip::new(path) {
+                Ok(c)  => c,
+                Err(e) => return Err(std::io::Error::new(std::io::ErrorKind::Other, e))
+            }
+        })
     }
 
-    pub fn add_key(&mut self, key: Key) {
-        self.mapping_table.push(key);
+    pub fn add_key(&mut self, index: usize, function: Box<dyn Fn(bool)>) -> Result<(), std::io::Error> {
+        let pin = {
+            if self.mini {
+                match index {
+                    0 => GpioV2Pins::P1_38 as u32,
+                    1 => GpioV2Pins::P1_11 as u32,
+                    2 => GpioV2Pins::P1_18 as u32,
+                    _ => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Invalid key index for device (Keybow mini)"))
+                }
+            } else {
+                match index {
+                    0  => GpioV2Pins::P1_38 as u32,
+                    1  => GpioV2Pins::P1_31 as u32,
+                    2  => GpioV2Pins::P1_15 as u32,
+                    3  => GpioV2Pins::P1_11 as u32,
+                    4  => GpioV2Pins::P1_36 as u32,
+                    5  => GpioV2Pins::P1_32 as u32,
+                    6  => GpioV2Pins::P1_18 as u32,
+                    7  => GpioV2Pins::P1_13 as u32,
+                    8  => GpioV2Pins::P1_37 as u32,
+                    9  => GpioV2Pins::P1_33 as u32,
+                    10 => GpioV2Pins::P1_29 as u32,
+                    11 => GpioV2Pins::P1_16 as u32,
+                    _ => return Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "Invalid key index for device (Keybow mini)"))
+                }
+            }
+        };
+        let line = match self._chip.get_line(pin) {
+            Ok(l)  => l,
+            Err(e) => return Err(std::io::Error::new(std::io::ErrorKind::Other, e))
+        };
+        self.mapping_table.push((line, function));
         self.states.push(false);
+        Ok(())
     }
 
     fn update_keys(&mut self) -> Result<(), std::io::Error> {
-        for (i, key) in self.mapping_table.iter_mut().enumerate() {
-            let state: bool = key.read_value()?.into();
-            if state != self.states[i] {
-                if let Some(function) = &key.function {
-                    function(state);
-                    self.states[i] = state;
-                }
+        for (index, (line, function)) in self.mapping_table.iter().enumerate() {
+            let handle = match line.request(gpio_cdev::LineRequestFlags::INPUT, 0, &format!("Keybow{}", if self.mini { " mini" } else { "" })) {
+                Ok(h)  => h,
+                Err(e) => return Err(std::io::Error::new(std::io::ErrorKind::Other, e))
+            };
+            match handle.get_value() {
+                Ok(v) => {
+                    let state = v == 0;
+                    if state != self.states[index] {
+                        function(state);
+                        self.states[index] = state;
+                    }
+                },
+                Err(e) => return Err(std::io::Error::new(std::io::ErrorKind::Other, e))
             }
         }
         Ok(())
     }
 }
 
-#[test]
-fn full() -> Result<(), std::io::Error> {
-    let mut k = crate::Keybow::new_mini();
-    let dummy = DummyPin::new(|| true);
-    k.add_key(
-        Key {
-            pin: Box::new(dummy),
-            function: Some(Box::new(|state: bool| println!("Prev Key: {}", state)))
-        }
-    );
-    k.update_keys().unwrap();
-}
+// #[test]
+// fn full() -> Result<(), std::io::Error> {
+//     let mut k = crate::Keybow::new_mini();
+//     let dummy = DummyPin::new(|| true);
+//     k.add_key(
+//         Key {
+//             pin: Box::new(dummy),
+//             function: Some(Box::new(|state: bool| println!("Prev Key: {}", state)))
+//         }
+//     );
+//     k.update_keys().unwrap();
+// }
